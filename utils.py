44a10acf0fab0ada1784acaebc4a7696e1bf04bc
ESWZY <1006460505@qq.com>
2020-04-16 16:55:26 +0800

    Fix: not keep <a> without text or link

--- utils.py
+++ utils.py
@@ -98,9 +98,14 @@ def keep_link(text, url):
 
             # Get plain text and concatenate with link
             result += keep_img(other[0], url)
-            if link_url:
-                link_url = get_full_link(link_url, url)
-                result += '<a href=\"' + link_url + '\">' + str(content) + '</a>'
+
+            # Not keep <a> without text or link
+            if content:
+                if link_url:
+                    link_url = get_full_link(link_url, url)
+                    result += '<a href=\"' + link_url + '\">' + str(content) + '</a>'
+                else:
+                    str(content)
 
             # Remove the processed text from processing string
             cp = str(cp).replace(str(other[0]) + str(link), "")
