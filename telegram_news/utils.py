ebd068e2115d3f574d80aa15219543f994bf23f9
ESWZY <1006460505@qq.com>
2020-10-20 14:21:47 +0800

    Enhance: robustness of video compression function

--- telegram_news/utils.py
+++ telegram_news/utils.py
@@ -395,18 +395,48 @@ def save_compressed_video(video_full_path, size_upper_bound, two_pass=True, file
     extension = '.mp4'
     output_file_name = filename + filename_suffix + extension
 
+    total_bitrate_lower_bound = 10000
+    min_audio_bitrate = 32000
+    max_audio_bitrate = 256000
+    min_video_bitrate = 100000
+
     try:
+        # Bitrate reference: https://en.wikipedia.org/wiki/Bit_rate#Encoding_bit_rate
         probe = ffmpeg.probe(video_full_path)
         # Video duration, in s.
         duration = float(probe['format']['duration'])
-        # Target audio bitrate, in bps.
+        # Audio bitrate, in bps.
         audio_bitrate = float(next((s for s in probe['streams'] if s['codec_type'] == 'audio'), None)['bit_rate'])
+        # Target total bitrate, in bps.
+        target_total_bitrate = (size_upper_bound * 1024 * 8) / (1.073741824 * duration)
+        if target_total_bitrate < total_bitrate_lower_bound:
+            print('Bitrate is extremely low! Stop compress!')
+            return False
+
+        # Best min size, in kB.
+        best_min_size = (min_audio_bitrate + min_video_bitrate) * (1.073741824 * duration) / (8 * 1024)
+        if size_upper_bound < best_min_size:
+            print('Quality not good! Recommended minimum size:', '{:,}'.format(int(best_min_size)), 'KB.')
+            # return False
+
+        # Target audio bitrate, in bps.
+        audio_bitrate = audio_bitrate
+
+        if target_total_bitrate < 11 * audio_bitrate:
+            audio_bitrate = target_total_bitrate / 11
+            if audio_bitrate < min_audio_bitrate:
+                audio_bitrate = min_audio_bitrate
+                if audio_bitrate > target_total_bitrate:
+                    audio_bitrate = target_total_bitrate / 11
+            elif audio_bitrate > max_audio_bitrate:
+                audio_bitrate = max_audio_bitrate
+
         # Target video bitrate, in bps.
-        video_bitrate = ((size_upper_bound * 1024 * 8) / (1.048576 * duration)) - audio_bitrate
-        # If audio size is bigger than `size_upper_bound`
-        if video_bitrate < 0:
-            audio_bitrate *= (size_upper_bound * 1024) / os.path.getsize(video_full_path)
-            video_bitrate = ((size_upper_bound * 1024 * 8) / (1.073741824 * duration)) - audio_bitrate
+        video_bitrate = target_total_bitrate - audio_bitrate
+        if video_bitrate < 1000:
+            # TODO: keep audio only?
+            print('Bitrate {} is extremely low! Stop compress.'.format(video_bitrate))
+            return False
 
         i = ffmpeg.input(video_full_path)
         if two_pass:
